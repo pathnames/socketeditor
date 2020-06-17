de478673c4051be689b2861527daf42f62e28693
pjkpjk900 <pk58@buffalo.edu>
2020-06-17 01:25:33 -0400

    Serving html file

---
+++ client.js
@@ -0,0 +1,6 @@
+const socket = io('http://localhost:3000');
+const editor = document.getElementById('editor');
+
+editor.addEventListener('keyup', (event) => {
+    
+});
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const socket = require('socket.io');
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log("Listening on port 3000!");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//Socket setup
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('Made socket connection at id: ' + socket.id);
 });
